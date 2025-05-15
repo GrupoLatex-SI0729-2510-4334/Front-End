eaedf485b3960400178c6d21e2c10ab6311e8b8b
Pedro Nanfuñay <pedro18nan@gmail.com>
2025-05-15 11:47:49 -0500

    feat(develop): added profiles endpoint in environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   serverBaseUrl: 'http://localhost:3000/api/v1',
   coursesEndpointPath: '/dashboard',
+  profilesEndpointPath: '/profiles',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   serverBaseUrl: 'http://localhost:3000/api/v1',
   coursesEndpointPath: '/',
+  profilesEndpointPath: '/profiles',
 };
