1ceb399883e00cbf4b25cde186a15f7da0355023
Pedro Nanfuñay <pedro18nan@gmail.com>
2025-06-13 23:38:44 -0500

    feat: added portfolio entity

---
+++ src/app/profile/model/portfolio.entity.ts
@@ -0,0 +1,17 @@
+export class Portfolio {
+  title: string;
+  description: string;
+  urlImage: string;
+  type: string;
+  url: string;
+  releasedAt: string;
+
+  constructor() {
+    this.title = '';
+    this.description = '';
+    this.urlImage = '';
+    this.type = '';
+    this.url = '';
+    this.releasedAt = '';
+  }
+}
